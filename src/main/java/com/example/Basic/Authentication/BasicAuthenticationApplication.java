ed7304856ecad27e1d05bc1f5cd90dbd8cd607f9
Arun <arunkumard0606@gmail.com>
2024-05-16 16:27:59 +0530

    Initial commit

---
+++ src/main/java/com/example/Basic/Authentication/BasicAuthenticationApplication.java
@@ -0,0 +1,14 @@
+package com.example.Basic.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthenticationApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
